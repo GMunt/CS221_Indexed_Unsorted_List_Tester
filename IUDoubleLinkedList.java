d67f67bbeb04ef0ce599dfc19ac40b76aeaed808
Gage Munt <gagemunt@u.boisestate.edu>
2025-03-27 13:07:54 -0600

    In Class Work 3/27/2025

--- IUDoubleLinkedList.java
+++ IUDoubleLinkedList.java
@@ -1,5 +1,6 @@
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /**
  * Double-linked-node-based implementation of IndexedUnsortedList
@@ -12,32 +13,69 @@ public class IUDoubleLinkedList<E> implements IndexedUnsortedList<E> {
     private int size;
     private int modCount;
 
+    public IUDoubleLinkedList() {
+        head = tail = null;
+        size = 0;
+        modCount = 0;
+    }
+
     @Override
-    public void addToFront(Object element) {
+    public void addToFront(E element) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'addToFront'");
     }
 
     @Override
-    public void addToRear(Object element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addToRear'");
+    public void addToRear(E element) {
+        Node<E> newNode = new Node<E>(element);
+        newNode.setPreviousNode(tail);
+        if (isEmpty()) {
+            head = newNode;
+        }
+        else {
+            tail.setNextNode(newNode);
+        }
+        tail = newNode;
+        size++;
+        modCount++;
     }
 
     @Override
-    public void add(Object element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    public void add(E element) {
+        addToRear(element);
     }
 
     @Override
-    public void addAfter(Object element, Object target) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addAfter'");
+    public void addAfter(E element, E target) {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node<E> nodeToAdd = new Node<E>(element);
+        Node<E> currentNode = head;
+        // Traverse
+        while (currentNode != null && !currentNode.getElement().equals(target)) {
+            currentNode = currentNode.getNextNode();
+        }
+        // if the target isn't found
+        if (currentNode == null) { 
+            throw new NoSuchElementException();
+        }
+        // Update Nodes 
+        nodeToAdd.setPreviousNode(currentNode);
+        nodeToAdd.setNextNode(currentNode.getNextNode());
+        currentNode.setNextNode(nodeToAdd);
+        if (nodeToAdd.getNextNode() == null) { // or currentNode == tail
+            tail = nodeToAdd;
+        }
+        else {
+            nodeToAdd.getNextNode().setPreviousNode(nodeToAdd);
+        }
+        size++;
+        modCount++;
     }
 
     @Override
-    public void add(int index, Object element) {
+    public void add(int index, E element) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
@@ -50,12 +88,24 @@ public class IUDoubleLinkedList<E> implements IndexedUnsortedList<E> {
 
     @Override
     public E removeLast() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        E retVal = tail.getElement();
+        if (head == tail) { // size == 1
+            head = null;
+        } 
+        else {
+            tail.getPreviousNode().setNextNode(null);
+        }
+        tail = tail.getPreviousNode();
+        size--;
+        modCount++;
+        return retVal;
     }
 
     @Override
-    public E remove(Object element) {
+    public E remove(E element) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'remove'");
     }
@@ -67,51 +117,83 @@ public class IUDoubleLinkedList<E> implements IndexedUnsortedList<E> {
     }
 
     @Override
-    public void set(int index, Object element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    public void set(int index, E element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<E> currentNode = head;
+        // Traverse to the index 
+        for (int i = 0; i < index; i++) {
+            currentNode.getNextNode();
+        }
+        // Update element
+        currentNode.setElement(element);
+        modCount++;
     }
 
     @Override
     public E get(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        if (index < 0 || index >=  size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.getNextNode();
+        }
+        return currentNode.getElement();
     }
 
     @Override
-    public int indexOf(Object element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'indexOf'");
+    public int indexOf(E element) {
+        Node<E> currentNode = head;
+        int currentIndex = 0;
+        boolean foundElement = false;
+        while (currentNode != null && !foundElement) {
+            if (currentNode.getElement().equals(element)) {
+                foundElement = true;
+            }
+            else {
+                currentNode = currentNode.getNextNode();
+                currentIndex++;
+            }    
+        }
+        if (!foundElement) {
+            currentIndex = -1;
+        }
+        return currentIndex;
     }
 
     @Override
     public E first() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'first'");
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return head.getElement();
     }
 
     @Override
     public E last() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'last'");
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return tail.getElement();
     }
 
     @Override
-    public boolean contains(Object target) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    public boolean contains(E target) {
+        return indexOf(target) > -1;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+        return size == 0; //best clarity 
+        // return head == null; //paranoia way 
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return size;
     }
 
     @Override
